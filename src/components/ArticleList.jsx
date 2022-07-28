7304b5b4933b1eae44a3be6d44460cbf4c8f6135
laurie60 <laurie8000@googlemail.com>
2022-07-28 10:18:16 +0100

    first commit

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,20 +1,23 @@
 import { useEffect, useState } from "react";
 import ArticleCard from "./ArticleCard";
 import classes from "./ArticleCard.module.css";
+import { useParams } from "react-router-dom";
 
 export default function ArticleList() {
   const [listArticles, setListArticles] = useState({});
   const [isLoading, setIsLoading] = useState(true);
+  const { topic } = useParams();
   useEffect(() => {
     setIsLoading(true);
-    fetch("https://laurences-news.herokuapp.com/api/articles")
-      .then((res) => {
-        return res.json();
-      })
-      .then((body) => {
-        setListArticles(body.articles);
-        setIsLoading(false);
-      });
+    if (topic)
+      fetch("https://laurences-news.herokuapp.com/api/articles")
+        .then((res) => {
+          return res.json();
+        })
+        .then((body) => {
+          setListArticles(body.articles);
+          setIsLoading(false);
+        });
   }, []);
   return isLoading ? (
     <p>Loading</p>
