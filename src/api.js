81c56dbeafe7b78f19983841cd12291e5405a6ef
laurie60 <laurie8000@googlemail.com>
2022-12-05 16:16:14 +0000

    include axios retry to inc reliability

--- src/api.js
+++ src/api.js
@@ -1,4 +1,7 @@
 import axios from "axios";
+import axiosRetry from "axios-retry";
+
+axiosRetry(axios, { retries: 5 });
 
 const URL = "https://be-news.cyclic.app/api";
 
@@ -11,29 +14,87 @@ export const getUsers = () => {
 };
 
 export const getComments = (arg) => {
-  console.log(arg, "ingetcomments");
-  return axios.get(`${URL}/articles/${arg}/comments`).then(({ data }) => {
-    console.log(data);
-    return data.comments;
+  axiosRetry(axios, {
+    retries: 10,
+    retryDelay: axiosRetry.exponentialDelay,
+    retryCondition: () => true,
   });
+
+  console.log(arg, "ingetcomments");
+  return axios
+    .get(`${URL}/articles/${arg}/comments`)
+    .then(({ data }) => {
+      console.log(data);
+      return data.comments;
+    })
+    .catch(function (error) {
+      console.log("in error");
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    });
 };
 
 export const getArticles = (arg) => {
+  axiosRetry(axios, {
+    retries: 10,
+    retryDelay: axiosRetry.exponentialDelay,
+    retryCondition: () => true,
+  });
   console.log(arg, "arg in getArticles");
   if (arg) {
-    return axios.get(`${URL}/articles?topic=${arg}`).then(({ data }) => {
-      return data.articles;
-    });
+    return axios
+      .get(`${URL}/articles?topic=${arg}`)
+      .then(({ data }) => {
+        return data.articles;
+      })
+
+      .catch(function (error) {
+        console.log("in error");
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
+        }
+        console.log(error.config);
+      });
   }
+
   return axios.get(`${URL}/articles`).then(({ data }) => {
     return data.articles;
   });
 };
 
 export const getArticle = (arg) => {
-  console.log(arg);
+  axiosRetry(axios, {
+    retries: 10,
+    retryDelay: axiosRetry.exponentialDelay,
+    retryCondition: () => true,
+  });
   return axios.get(`${URL}/articles/${arg}`).then(({ data }) => {
-    console.log(data, "<<<<data");
     return data.article;
   });
 };
